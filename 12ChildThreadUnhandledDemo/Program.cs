55fbad9eff5e7bb4a8426b5af454799c81f3c6a3
Francis Chung <francis.c.chung@gmail.com>
2020-01-13 10:33:15 +1100

    Unhandled Thread Exception .NET Core Example

---
+++ 12ChildThreadUnhandledDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+namespace _12ChildThreadUnhandledDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChildThreadUnhandled.runTest();
+            //Console.ReadKey();
+        }
+
+        public class ChildThreadUnhandled
+        {
+            public static void runTest()
+            {
+
+                Thread thread = new Thread(() =>
+                {
+
+                    // Throw an unhandled exception
+                    throw new Exception("");
+
+                });
+
+
+                thread.Start();
+                thread.Join();
+
+                Console.WriteLine("Alive");
+
+            }
+        }
+    }
+}
