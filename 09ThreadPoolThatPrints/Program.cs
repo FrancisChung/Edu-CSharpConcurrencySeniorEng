e17bca82885b49f85bb69e37071def0e23a7e0fc
Francis Chung <francis.c.chung@gmail.com>
2020-01-13 09:15:35 +1100

    ThreadPool that Prints example

--- 09ThreadPoolThatPrints/Program.cs
+++ 09ThreadPoolThatPrints/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
-
+using System.Threading;
 namespace _09ThreadPoolThatPrints
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new ThreadPoolExample().RunTest();
+            //Console.ReadKey();
+        }
+    }
+
+    class ThreadPoolExample
+    {
+        EventWaitHandle ewh = new EventWaitHandle(false, EventResetMode.AutoReset);
+        void Work(object State)
+        {
+            Thread.Sleep(2000);
+            Console.WriteLine("Hello, is it me you're waiting for?");
+            ewh.Set();
+        }
+
+        public void RunTest()
+        {
+            ThreadPool.QueueUserWorkItem(Work);
+            ewh.WaitOne();
         }
     }
 }
