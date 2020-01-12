5ef95dd807d43402a3b0702ed13be501ca7bfd9e
Francis Chung <francis.c.chung@gmail.com>
2020-01-13 10:33:38 +1100

    Unhandled Thread Exception .NET 4.7.2 Example

---
+++ 13ThreadAbortUnhandledNet472Demo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace _13ThreadAbortUnhandledNet472Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChildThreadUnhandled.runTest();
+            Console.ReadKey();
+        }
+
+        public class ChildThreadUnhandled
+        {
+            public static void runTest()
+            {
+
+                Thread thread = new Thread(() =>
+                {
+                    Console.WriteLine("Thread Aborting");
+                    Thread.CurrentThread.Abort();
+
+                });
+
+
+                thread.Start();
+                thread.Join();
+
+                Console.WriteLine("Aborted but Alive");
+
+            }
+        }
+    }
+}
