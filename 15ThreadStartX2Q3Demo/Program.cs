20434ce3b970751c9191e864682ca12c6e005888
Francis Chung <francis.c.chung@gmail.com>
2020-01-13 10:42:13 +1100

    Q3 Thread Start x2 Example

---
+++ 15ThreadStartX2Q3Demo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace _15ThreadStartX2Q3Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().RunTest();
+            Console.ReadKey();
+        }
+
+        void greet()
+        {
+            Console.WriteLine("Hi");
+        }
+
+        public void RunTest()
+        {
+            Thread thread = new Thread(() =>
+            {
+                greet();
+            });
+
+            thread.Start();
+            thread.Join();
+
+            thread.Start();
+            thread.Join();
+        }
+    }
+}
