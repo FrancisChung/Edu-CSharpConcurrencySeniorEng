700d2e825c99b16bf078802a2935213de76a8f1a
Francis Chung <francis.c.chung@gmail.com>
2020-01-15 15:00:39 +1100

    Mutex Q3 - AbandonedMutexException!

---
+++ 19MutexQ3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace _19MutexQ3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Environment.OSVersion);
+            Console.WriteLine(Environment.Version);
+
+            Mutex mutex = new Mutex();
+            Thread t1 = new Thread(() => {
+
+                mutex.WaitOne();
+                Console.WriteLine("t1 exiting");
+            });
+            t1.Start();
+            t1.Join();
+
+            Thread t2 = new Thread(() => {
+                mutex.WaitOne();
+                Console.WriteLine("t2 exiting");
+            });
+            t2.Start();
+            t2.Join();
+        }
+    }
+}
