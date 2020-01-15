1852af56b728231776472a920f5a63d4bd75e8d8
Francis Chung <francis.c.chung@gmail.com>
2020-01-15 14:19:58 +1100

    Mutex Q2

---
+++ 18MutexQ2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+namespace _18MutexQ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mutex mutex = new Mutex();
+            mutex.WaitOne();
+            Console.WriteLine("Program Exiting");
+        }
+    }
+}
