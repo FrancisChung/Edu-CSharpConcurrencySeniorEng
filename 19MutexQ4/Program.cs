3c4aa52de2b84fe8a4257ebb81051a7bdc23c09a
Francis Chung <francis.c.chung@gmail.com>
2020-01-15 15:36:50 +1100

    Mutex Q4

---
+++ 19MutexQ4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace _19MutexQ4
+{
+    class Program
+    {
+        static void DoImportantWork()
+        {
+            throw new SystemException();
+        }
+
+        static void Main()
+        {
+            Mutex mutex = new Mutex();
+
+            Thread t1 = new Thread(() => {
+                try
+                {
+                    mutex.WaitOne();
+                    DoImportantWork();
+                }
+                catch (Exception)
+                {
+                    // swallow exception
+                    Console.WriteLine("Swallowing exception");
+                }
+                finally
+                {
+                    mutex.ReleaseMutex();
+                }
+            });
+
+            t1.Start();
+            t1.Join();
+
+            Console.ReadKey();
+        }
+    }
+}
