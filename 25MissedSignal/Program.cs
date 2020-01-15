78c57dff42edb8c0cec00995a3ec5a0751e1f082
Francis Chung <francis.c.chung@gmail.com>
2020-01-16 10:54:53 +1100

    Missed Signal Example

---
+++ 25MissedSignal/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+
+namespace _25MissedSignal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new MissedSignal().runTest();
+        }
+
+        public class MissedSignal
+        {
+
+            object padlock = new object();
+
+            void thread1()
+            {
+                Monitor.Enter(padlock);
+                Monitor.Wait(padlock);
+                Monitor.Exit(padlock);
+            }
+
+
+            void thread2()
+            {
+                Monitor.Enter(padlock);
+                Monitor.Pulse(padlock);
+                Monitor.Exit(padlock);
+            }
+
+
+
+            public void runTest()
+            {
+
+                Thread t1 = new Thread(() =>
+                {
+                    thread1();
+                });
+
+                Thread t2 = new Thread(() =>
+                {
+                    thread2();
+                });
+
+                t2.Start();
+                t2.Join();
+
+                t1.Start();
+                t2.Join();
+            }
+        }
+    }
+
+
+}
