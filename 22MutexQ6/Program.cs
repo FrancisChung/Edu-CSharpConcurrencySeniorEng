0264e0c06f4c630f64a38fd11fb08a387aaeb740
Francis Chung <francis.c.chung@gmail.com>
2020-01-15 17:15:35 +1100

    Mutex Q6

---
+++ 22MutexQ6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace _22MutexQ6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var mutex = new Mutex())
+            {
+                mutex.WaitOne();
+                Console.WriteLine("WaitOne");
+                Thread.Sleep(500);
+                mutex.ReleaseMutex();
+                Console.WriteLine("ReleaseMutex");
+            }
+            Console.ReadKey();
+        }
+    }
+}
