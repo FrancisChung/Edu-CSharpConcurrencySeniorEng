f9ee6fccbe0d50bd6e8274ced305324d2e499932
Francis Chung <francis.c.chung@gmail.com>
2020-01-15 17:23:36 +1100

    Mutex Q7

---
+++ 23MutexQ7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace _23MutexQ7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mutex mutex = new Mutex();
+
+            Thread t1 = new Thread(() =>
+            {
+                mutex.WaitOne();
+                mutex.WaitOne();
+                mutex.ReleaseMutex();
+                //mutex.ReleaseMutex();
+
+            });
+
+            t1.Start();
+            t1.Join();
+
+            // Main thread attemps to acquire the mutex
+            mutex.WaitOne();
+            Console.WriteLine("All Good");
+            mutex.ReleaseMutex();
+            Console.ReadKey();
+        }
+    }
+}
