6dbc46f52144faa6d60cf43505a7fa700051fb1d
Francis Chung <francis.c.chung@gmail.com>
2020-01-15 16:53:03 +1100

    Mutex Q5

---
+++ 21MutexQ5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace _21MutexQ5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mutex mutex = new Mutex();
+
+            Thread t1 = new Thread(() =>
+            {
+                try
+                {
+                    lock (mutex)
+                    {
+                        Thread.Sleep(500);
+                    }
+                }
+                catch (Exception)
+                {
+                    // swallow exception
+                    Console.WriteLine("exception swallowed");
+                }
+            });
+
+            t1.Start();
+            t1.Join();
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+    }
+}
