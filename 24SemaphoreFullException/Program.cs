8655fed52d66be9ab145419494cf0a487e14ecb9
Francis Chung <francis.c.chung@gmail.com>
2020-01-16 10:43:26 +1100

    SemaphoreFullException Example

---
+++ 24SemaphoreFullException/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace _24SemaphoreFullException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Semaphore sem = new Semaphore(0, 1);
+            sem.Release();
+            sem.Release();
+            Console.ReadKey();
+        }
+
+
+    }
+}
