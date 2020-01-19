0895c3b5e2678dac48f0b3d71cfda4701f6028eb
Francis Chung <francis.c.chung@gmail.com>
2020-01-19 16:50:14 +1100

    Semaphore Ping Pong Q2

---
+++ 28SemaphorePingPong2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+
+namespace _28SemaphorePingPong2
+{
+    // Can't be solved with a single semaphore
+    public class SemaphorePingPongExample2
+    {
+        Semaphore sem = new Semaphore(0, 1);
+        static void Main()
+        {
+            new SemaphorePingPongExample2().runTest();
+            Console.ReadKey();
+        }
+
+        void pong()
+        {
+            while (true)
+            {
+                sem.WaitOne();
+                Console.WriteLine("Pong");
+                Thread.Sleep(400);
+                sem.Release();
+            }
+        }
+
+
+        void ping()
+        {
+            while (true)
+            {
+                Console.WriteLine("Ping");
+                sem.Release();
+                Thread.Sleep(400);
+
+                sem.WaitOne();
+            }
+        }
+
+
+        public void runTest()
+        {
+            Thread pingThread = new Thread(() =>
+            {
+                ping();
+            });
+
+
+            Thread pongThread = new Thread(() =>
+            {
+                pong();
+            });
+
+            pongThread.Start();
+            pingThread.Start();
+
+            pongThread.Join();
+            pingThread.Join();
+        }
+    }
+}
