0cd4bf388463ea0d53a8adc41711f7949a69f69b
Francis Chung <francis.c.chung@gmail.com>
2020-01-20 09:10:35 +1100

    Interlocked PingPong Demo

---
+++ 31InterlockedPingPong/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+
+namespace _31InterlockedPingPong
+{
+
+
+    class Demonstration
+    {
+        static void Main()
+        {
+            new PingPongInterlockedExample().runTest();
+            Console.ReadKey();
+        }
+    }
+
+    public class PingPongInterlockedExample
+    {
+        long flag = 1;
+
+        void ping()
+        {
+            while (true)
+            {
+                while (Interlocked.Read(ref flag) == 1) { }
+
+                Console.WriteLine("Ping");
+                Thread.Sleep(400);
+                Interlocked.Exchange(ref flag, 1);
+
+            }
+        }
+
+
+        void pong()
+        {
+            while (true)
+            {
+                while (Interlocked.Read(ref flag) == 0) { }
+
+                Console.WriteLine("Pong");
+                Thread.Sleep(400);
+                Interlocked.Exchange(ref flag, 0);
+            }
+
+        }
+
+        public void runTest()
+        {
+            Thread pingThread = new Thread(() =>
+            {
+                ping();
+            });
+
+
+            Thread pongThread = new Thread(() =>
+            {
+                pong();
+            });
+
+
+            pingThread.IsBackground = true;
+            pongThread.IsBackground = true;
+
+            pingThread.Start();
+            pongThread.Start();
+
+            Thread.Sleep(1000);
+        }
+    }
+}
