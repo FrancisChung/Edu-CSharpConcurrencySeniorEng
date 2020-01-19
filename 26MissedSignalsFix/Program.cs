886d8770e4d8f268b27c0c6c4a345bc23d40fb75
Francis Chung <francis.c.chung@gmail.com>
2020-01-19 16:29:19 +1100

    Missed Signals Fix

---
+++ 26MissedSignalsFix/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+
+namespace _26MissedSignalsFix
+{
+    class Demonstration
+    {
+        static void Main()
+        {
+            new FixedMissedSingal().runTest();
+        }
+    }
+
+    public class FixedMissedSingal
+    {
+
+        Semaphore sem = new Semaphore(0, 1);
+
+        void thread1()
+        {
+            Console.WriteLine("WaitOne Pre");
+            sem.WaitOne();
+            Console.WriteLine("WaitOne Post");
+
+        }
+
+
+        void thread2()
+        {
+
+            sem.Release();
+        }
+
+
+
+        public void runTest()
+        {
+
+            Thread t1 = new Thread(() =>
+            {
+                thread1();
+            });
+
+            Thread t2 = new Thread(() =>
+            {
+                thread2();
+            });
+
+            t2.Start();
+            t2.Join();
+
+            t1.Start();
+            t2.Join();
+
+            Console.ReadKey();
+        }
+    }
+}
