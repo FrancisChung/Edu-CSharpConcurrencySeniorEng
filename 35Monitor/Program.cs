3f3711e25d470187131645df596d352d11d12c85
Francis Chung <francis.c.chung@gmail.com>
2020-01-22 08:27:05 +1100

    Monitor Example

---
+++ 35Monitor/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+
+namespace _35Monitor
+{
+    class Program
+    {
+
+
+        class Demonstration
+        {
+            static void Main()
+            {
+                Demonstration demo = new Demonstration();
+                Stopwatch stopwatch = Stopwatch.StartNew();
+
+                ThreadStart threadStart = new ThreadStart(demo.example);
+                Thread thread1 = new Thread(threadStart);
+                Thread thread2 = new Thread(threadStart);
+                Thread thread3 = new Thread(threadStart);
+
+                thread1.Start();
+                Thread.Sleep(500);
+                thread2.Start();
+                Thread.Sleep(500);
+                thread3.Start();
+
+                thread3.Join();
+                thread2.Join();
+                thread1.Join();
+
+                stopwatch.Stop();
+                Console.WriteLine("\n Total execution time of program in seconds : " + stopwatch.ElapsedMilliseconds);
+                Console.ReadKey();
+            }
+
+            public void example()
+            {
+                Monitor.Enter(this);
+
+                // Critical section
+                Console.WriteLine("Thread with id " + Thread.CurrentThread.ManagedThreadId + " enters the critical section.");
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread with id " + Thread.CurrentThread.ManagedThreadId + " exits the critical section.");
+
+                Monitor.Exit(this);
+            }
+
+        }
+    }
+}
