f2f9de0b2d2bf70d8ab739ede2c2a88438b2a5fd
Francis Chung <francis.c.chung@gmail.com>
2020-01-22 08:31:46 +1100

    Monitor.Wait() Exception example

---
+++ 36MonitorWaitException/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace _36MonitorWaitException
+{
+    class Program
+    {
+        static void Main()
+        {
+            Object obj = new object();
+
+            // Monitor.Enter(obj);
+
+            Monitor.Wait(obj);  //Can't be called from Unsynchronised block of code
+
+            // Monitor.Exit(obj);
+
+        }
+    }
+}
