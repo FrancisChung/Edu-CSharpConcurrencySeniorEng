cfae4b9f0773abb648fc2f321208ddf1d28b0b22
Francis Chung <francis.c.chung@gmail.com>
2020-01-25 12:58:55 +1100

    Explicitly converting a value to an Object first before using Monitor works.

---
+++ 40ValueTypeMonitorWorks/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+namespace _40ValueTypeMonitorWorks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valueType = 5;
+            Object valueTypeBoxed = valueType;
+
+            Monitor.Enter(valueTypeBoxed);
+
+            // Singal the monitor
+            Monitor.Pulse(valueTypeBoxed);
+
+            Monitor.Exit(valueTypeBoxed);
+
+            Console.WriteLine("This works but not good practice");
+            Console.ReadKey();
+        }
+    }
+}
