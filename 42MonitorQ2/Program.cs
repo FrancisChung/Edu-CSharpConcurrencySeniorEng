29c5d6db4026dfba3700fa692704b5f369bc43d9
Francis Chung <francis.c.chung@gmail.com>
2020-01-25 14:56:17 +1100

    Monitor Q2 (Can Enter Twice!)

---
+++ 42MonitorQ2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace _42MonitorQ2
+{
+    public class QuizQuestion
+    {
+        private readonly Object obj = new Object();
+
+        public void enterTwice()
+        {
+
+            Monitor.Enter(obj);
+            Monitor.Enter(obj);
+            Console.WriteLine("Hello");
+            Monitor.Exit(obj);
+        }
+
+        public static void Main()
+        {
+            Thread thread = new Thread(new ThreadStart(new QuizQuestion().enterTwice));
+            thread.Start();
+            thread.Join();
+        }
+    }
+}
