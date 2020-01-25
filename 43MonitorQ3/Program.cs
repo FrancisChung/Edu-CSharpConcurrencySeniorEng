18f673811eb911debbddf584187284ea3e3332e0
Francis Chung <francis.c.chung@gmail.com>
2020-01-25 15:09:22 +1100

    Monitor Q3 - Site says C) but I say its A) thread 2 is blocked

---
+++ 43MonitorQ3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+
+namespace _43MonitorQ3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new QuizQuestion().run();
+            Console.ReadKey();
+        }
+
+        public class QuizQuestion
+        {
+            private readonly Object obj = new Object();
+
+            public void enterTwice()
+            {
+
+                Monitor.Enter(obj);
+                Monitor.Enter(obj);
+                Console.WriteLine("Hello");
+                Monitor.Exit(obj);
+            }
+
+            public void enterOnce()
+            {
+                Monitor.Enter(obj);
+                Console.WriteLine("World");
+                Monitor.Exit(obj);
+            }
+
+            public void run()
+            {
+                Thread thread1 = new Thread(new ThreadStart(enterTwice));
+                thread1.Start();
+                thread1.Join();
+                Console.WriteLine("Thread 1 join");
+
+
+
+                Thread thread2 = new Thread(new ThreadStart(enterOnce));
+                thread2.Start();
+                Console.WriteLine("Thread 2 started");
+
+                thread2.Join();
+                Console.WriteLine("Thread 2 join");
+
+            }
+        }
+    }
+}
