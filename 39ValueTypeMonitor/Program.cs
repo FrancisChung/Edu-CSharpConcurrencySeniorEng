3377ee62ee1c670a7f0f522e4664a73de5e6dddc
Francis Chung <francis.c.chung@gmail.com>
2020-01-25 12:55:31 +1100

    Value Types as Synch Objects causes .SynchronizationLockException

---
+++ 39ValueTypeMonitor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace _39ValueTypeMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valueType = 5;
+
+            Monitor.Enter(valueType);
+            Monitor.Pulse(valueType);
+            Monitor.Exit(valueType);
+
+            Console.WriteLine("This should not work");
+            Console.ReadLine();
+        }
+    }
+}
