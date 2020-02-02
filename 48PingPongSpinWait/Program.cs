76398c350c0707cd0ecb73f69c62e0ce638dbfb0
Francis Chung <francis.c.chung@gmail.com>
2020-02-03 09:58:56 +1100

    Ping Pong SpinWait Example

---
+++ 48PingPongSpinWait/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading;
+
+namespace _48PingPongSpinWait
+{
+    class Demonstration
+    {
+        static void Main()
+        {
+            new PingPongSpinWait().runTest();
+        }
+    }
+
+    public class PingPongSpinWait
+    {
+        private volatile bool flag = true;
+
+        void ping()
+        {
+            SpinWait sw = new SpinWait();
+            while (true)
+            {
+
+
+                while (flag)
+                {
+                    sw.SpinOnce();
+                }
+
+                Console.WriteLine("Ping");
+                //Thread.Sleep(400);
+                flag = true;
+
+            }
+        }
+
+
+        void pong()
+        {
+            while (true)
+            {
+                SpinWait sw = new SpinWait();
+                while (!flag)
+                {
+                    sw.SpinOnce();
+                }
+
+                Console.WriteLine("Pong");
+                //Thread.Sleep(400);
+                flag = false;
+            }
+        }
+
+
+        public void runTest()
+        {
+
+            Thread pingThread = new Thread(() =>
+            {
+                ping();
+            });
+
+
+            Thread pongThread = new Thread(() =>
+            {
+                pong();
+            });
+
+
+            pingThread.IsBackground = true;
+            pongThread.IsBackground = true;
+
+            pingThread.Start();
+            pongThread.Start();
+
+            Thread.Sleep(1000);
+        }
+    }
+
+}
