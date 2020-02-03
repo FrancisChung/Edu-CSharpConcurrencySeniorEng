d0916a72f28d836953ccda76e4ea0820faf6c592
Francis Chung <francis.c.chung@gmail.com>
2020-02-03 20:02:07 +1100

    Barrier Example

---
+++ 49Barrier/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+
+
+namespace _49Barrier
+{
+
+    class Demonstration
+    {
+        static void Main()
+        {
+            new BarrierExample().runTest();
+        }
+    }
+
+    public class BarrierExample
+    {
+        Barrier barrier = new Barrier(3, (Barrier barrier) =>
+        {
+            Console.WriteLine("All threads reached at barrier");
+        });
+
+
+        void work()
+        {
+
+            barrier.SignalAndWait();
+            Console.WriteLine("Thread " + Thread.CurrentThread.ManagedThreadId + " released");
+            Thread.Sleep(1000);
+
+            barrier.SignalAndWait();
+            Console.WriteLine("Thread " + Thread.CurrentThread.ManagedThreadId + " released");
+            Thread.Sleep(1000);
+
+            barrier.SignalAndWait();
+            Console.WriteLine("Thread " + Thread.CurrentThread.ManagedThreadId + " released");
+        }
+
+        public void runTest()
+        {
+
+            Thread[] threads = new Thread[3];
+
+            for (int i = 0; i < 3; i++)
+                threads[i] = new Thread(() =>
+                {
+                    work();
+                });
+
+            for (int i = 0; i < 3; i++)
+                threads[i].Start();
+
+            for (int i = 0; i < 3; i++)
+                threads[i].Join();
+
+        }
+    }
+}
