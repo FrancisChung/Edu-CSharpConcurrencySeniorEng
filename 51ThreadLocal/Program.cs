37ed4f3f3aa17859c1e989fc64eeb24b1eedfad5
FrancisChung <francis.c.chung@gmail.com>
2020-02-21 11:48:00 +1100

    ThreadLocal Example

---
+++ 51ThreadLocal/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace _51ThreadLocal
+{
+    class Demonstration
+    {
+        static void Main()
+        {
+            new ThreadLocalExample().runTest();
+            Console.ReadKey();
+        }
+    }
+
+    public class ThreadLocalExample
+    {
+
+        ThreadLocal<int> threadValue = new ThreadLocal<int>();
+        Barrier barrier = new Barrier(5);
+
+        void work(object obj)
+        {
+            int i = (int)obj;
+            threadValue.Value = i * i;
+            barrier.SignalAndWait();
+
+            Console.WriteLine("Thread with id " + Thread.CurrentThread.ManagedThreadId + " computed square = " + threadValue.Value);
+
+        }
+
+        public void runTest()
+        {
+
+            Thread[] threads = new Thread[5];
+
+            for (int i = 0; i < 5; i++)
+                threads[i] = new Thread(new ParameterizedThreadStart(this.work));
+
+            for (int i = 0; i < 5; i++)
+                threads[i].Start(i + 1);
+
+            for (int i = 0; i < 5; i++)
+                threads[i].Join();
+
+        }
+    }
+}
