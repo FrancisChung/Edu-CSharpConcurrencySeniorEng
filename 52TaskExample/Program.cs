171aecca77f111237ce906f95a4f151884a53416
FrancisChung <francis.c.chung@gmail.com>
2020-02-24 08:35:55 +1100

    Basic Task Example

---
+++ 52TaskExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace _52TaskExample
+{
+
+    class Demonstration
+    {
+        static void Main()
+        {
+            new TaskExample().runTest();
+        }
+    }
+
+    public class TaskExample
+    {
+        public void runTest()
+        {
+
+            Task task = new Task(() => {
+                Console.WriteLine("Hello World");
+            });
+
+            task.Start();
+            task.Wait();
+            Console.ReadKey();
+
+        }
+    }
+}
