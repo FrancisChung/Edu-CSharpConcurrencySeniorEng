1060a96dae46f94221467298b2270be016ca770f
FrancisChung <francis.c.chung@gmail.com>
2020-02-24 08:54:16 +1100

    Task Factory StartNew Demo

---
+++ 54TaskFactoryDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _54TaskFactoryDemo
+{
+
+
+    class Demonstration
+    {
+        static void Main()
+        {
+            Task task = Task.Factory.StartNew((object obj) => {
+
+                Console.WriteLine("Hello " + obj);
+
+            }, "Reader");
+            task.Wait();
+        }
+    }
+}
