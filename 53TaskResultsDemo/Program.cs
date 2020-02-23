5946b7d7b9556e91922edb62da10ae2c1ab3b112
FrancisChung <francis.c.chung@gmail.com>
2020-02-24 08:44:06 +1100

    Task Results Demo

---
+++ 53TaskResultsDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _53TaskResultsDemo
+{
+    class Demonstration
+    {
+        static void Main()
+        {
+            new TaskExample().runTest();
+        }
+    }
+
+    public class TaskExample
+    {
+        public void runTest()
+        {
+
+            Task<String> task = new Task<String>((Object obj) =>
+            {
+                String name = (String)obj;
+                Console.WriteLine("Hello " + name);
+                Thread.Sleep(5000);
+                return "Task Completed Successfully";
+            }, "Reader", TaskCreationOptions.None);
+            task.Start();
+            Console.WriteLine(task.Result);
+        }
+    }
+}
