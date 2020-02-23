bef00ae4424be730f387e82c1a577f42a0fffa0b
FrancisChung <francis.c.chung@gmail.com>
2020-02-24 08:58:32 +1100

    Task Continuation Demo

---
+++ 55TaskContinuationDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _55TaskContinuationDemo
+{
+    class Demonstration
+    {
+        static void Main()
+        {
+            Task task1 = Task.Factory.StartNew((object obj) =>
+            {
+                Console.WriteLine("Hello " + obj);
+                Console.WriteLine("First task executed by thread with id " + Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(1000);
+
+            }, "Reader");
+
+            Task task2 = task1.ContinueWith((prevTask) =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Second task in progress");
+                Console.WriteLine("Second task executed by thread with id " + Thread.CurrentThread.ManagedThreadId);
+            });
+
+            Thread.Sleep(100);
+
+            Task.WaitAll(task1, task2);
+            Console.WriteLine("Both tasks finished");
+            Console.ReadKey();
+        }
+    }
+}
