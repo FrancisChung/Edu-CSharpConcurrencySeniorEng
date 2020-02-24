52d87a139e4c40aa283f5088c73bca2c2a14b53c
FrancisChung <francis.c.chung@gmail.com>
2020-02-25 07:30:53 +1100

    Memory Barrier Example 1

---
+++ 57MemoryBarrier1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+
+namespace _56MemoryBarrier1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new PossibleReorderingExample().runTest();
+            Console.WriteLine("Terminated");
+            Console.ReadKey();
+        }
+    }
+
+    public class PossibleReorderingExample
+    {
+        bool keepGoing = true;
+
+        public void work()
+        {
+            while (keepGoing)
+            {
+                Console.WriteLine("Doing something important");
+                Thread.MemoryBarrier();
+            }
+        }
+
+
+        public void runTest()
+        {
+
+            Thread childThread = new Thread(() =>
+            {
+                work();
+            });
+            childThread.Start();
+
+
+            // let child thread run for a second
+            Thread.Sleep(1000);
+
+            // update the flag
+            keepGoing = false;
+            Thread.MemoryBarrier();
+
+            // wait for child thread to terminate
+            childThread.Join();
+
+        }
+    }
+}
