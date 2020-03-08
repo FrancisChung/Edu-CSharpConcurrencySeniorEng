6788dfea6f41619dbf169f96cd4f9182e38619b8
FrancisChung <francis.c.chung@gmail.com>
2020-03-08 16:47:39 +1100

    Async Sleep Demo 1

---
+++ 60AsyncSleepDemo1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+namespace _60AsyncSleepDemo1
+{
+    class Demonstration
+    {
+        static async void sleep()
+        {
+            // wait for three second
+            Thread.Sleep(2000);
+        }
+
+        static void Main()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            sleep();
+            sw.Stop();
+            Console.WriteLine("Main thread id " + Thread.CurrentThread.ManagedThreadId + " program execution took = " + (sw.ElapsedMilliseconds / 1000.0) + " seconds");
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
