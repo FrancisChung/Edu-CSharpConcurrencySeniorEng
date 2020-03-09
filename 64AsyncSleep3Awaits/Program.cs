eb26b6b64131194945c6e2dd5fbc9e2dbdc19c69
FrancisChung <francis.c.chung@gmail.com>
2020-03-09 17:18:53 +1100

    Sometimes it runs on same thread, sometimes on different threads.

---
+++ 64AsyncSleep3Awaits/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _64AsyncSleep3Awaits
+{
+    class Demonstration
+    {
+        static async Task sleep()
+        {
+
+            // wait for 1 second
+            await Task.Delay(1000);
+            Console.WriteLine("Await Thread id " + Thread.CurrentThread.ManagedThreadId);
+
+            // wait for 1 second
+            await Task.Delay(1000);
+            Console.WriteLine("Await Thread id " + Thread.CurrentThread.ManagedThreadId);
+
+            // wait for 1 second
+            await Task.Delay(1000);
+            Console.WriteLine("Await Thread id " + Thread.CurrentThread.ManagedThreadId);
+        }
+
+        static void Main()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            Task sleepTask = sleep();
+            sleepTask.Wait();
+            sw.Stop();
+            Console.WriteLine("Main thread id " + Thread.CurrentThread.ManagedThreadId + " program execution took = " + (sw.ElapsedMilliseconds / 1000.0) + " seconds");
+
+        }
+    }
+}
