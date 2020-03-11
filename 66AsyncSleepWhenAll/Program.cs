91adde523181ccde6a207ad68e40a36ba7391369
FrancisChung <francis.c.chung@gmail.com>
2020-03-12 10:58:31 +1100

    WhenAll still the sames as 3 Task Waits

---
+++ 66AsyncSleepWhenAll/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _66AsyncSleepWhenAll
+{
+    class Demonstration
+    {
+        static async Task sleep()
+        {
+            // wait for 1 second
+            await Task.Delay(1000);
+            Console.WriteLine("Thread id " + Thread.CurrentThread.ManagedThreadId);
+
+            // wait for 1 second
+            await Task.Delay(1000);
+            Console.WriteLine("Thread id " + Thread.CurrentThread.ManagedThreadId);
+
+            // wait for 1 second
+            await Task.Delay(1000);
+            Console.WriteLine("Thread id " + Thread.CurrentThread.ManagedThreadId);
+        }
+        1
+        static void Main()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            Task superSleep = SuperSleep();
+            superSleep.Wait();
+
+            sw.Stop();
+            Console.WriteLine("Main thread id " + Thread.CurrentThread.ManagedThreadId + " program execution took = " + (sw.ElapsedMilliseconds / 1000.0) + " seconds");
+        }
+
+        private static async Task SuperSleep()
+        {
+            Task t1 = sleep();
+            Task t2 = sleep();
+            Task t3 = sleep();
+
+            Task combinedTask = Task.WhenAll(t1, t2, t3);
+            await combinedTask;
+        }
+    }
+}
